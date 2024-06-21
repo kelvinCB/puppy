75f381fb01b237012cbf520117ef11cd74d7588b
Kelvin <kelvinr02@hotmail.com>
2024-06-20 20:43:07 -0400

    Adding BigBark

--- puppy.go
+++ puppy.go
@@ -1,5 +1,7 @@
 package puppy
 
+import "github.com/GoesToEleven/dog"
+
 func Bark() string {
 	return "Woof!"
 }
@@ -8,6 +10,10 @@ func Barks() string {
 	return "Woof! Woof! Woof!"
 }
 
-func BigBark() string{
-	return ""
+func BigBark() string {
+	return dog.WhenGrownUp(Bark())
+}
+
+func BigBarks() string {
+	return dog.WhenGrownUp(Barks())
 }
